fc62b798e382ab61fd8dd0c69c49468e58227972
osawa-koki <github@osawa.dev>
2023-02-18 21:07:18 +0900

    小さな修正。

--- hello_world/app.py
+++ hello_world/app.py
@@ -27,48 +27,61 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # パラメタの取得
-    width = (int)(event.get(QUERY_STRING_PARAMETERS).get('width') or 512)
-    height = (int)(event.get(QUERY_STRING_PARAMETERS).get('height') or 512)
-    x_min = (float)(event.get(QUERY_STRING_PARAMETERS).get('x_min') or -2.2)
-    x_max = (float)(event.get(QUERY_STRING_PARAMETERS).get('x_max') or 1.8)
-    y_min = (float)(event.get(QUERY_STRING_PARAMETERS).get('y_min') or -2.0)
-    y_max = (float)(event.get(QUERY_STRING_PARAMETERS).get('y_max') or 2.0)
-    iterations = (int)(event.get(QUERY_STRING_PARAMETERS).get('iterations') or 100)
-    threshold = (int)(event.get(QUERY_STRING_PARAMETERS).get('threshold') or 10)
+    try:
+        # パラメタの取得
+        # width = (int)(event.get(QUERY_STRING_PARAMETERS).get('width') or 512)
+        # height = (int)(event.get(QUERY_STRING_PARAMETERS).get('height') or 512)
+        # x_min = (float)(event.get(QUERY_STRING_PARAMETERS).get('x_min') or -2.2)
+        # x_max = (float)(event.get(QUERY_STRING_PARAMETERS).get('x_max') or 1.8)
+        # y_min = (float)(event.get(QUERY_STRING_PARAMETERS).get('y_min') or -2.0)
+        # y_max = (float)(event.get(QUERY_STRING_PARAMETERS).get('y_max') or 2.0)
+        # iterations = (int)(event.get(QUERY_STRING_PARAMETERS).get('iterations') or 100)
+        # threshold = (int)(event.get(QUERY_STRING_PARAMETERS).get('threshold') or 10)
 
-    # 画像を作成
-    image = Image.new('RGB', (width, height), (0, 0, 0))
+        width = 512
+        height = 512
+        x_min = -2.2
+        x_max = 1.8
+        y_min = -2.0
+        y_max = 2.0
+        iterations = 100
+        threshold = 10
 
-    # マンデルブロ集合を描写
-    for x in range(width):
-        for y in range(height):
-            # 座標を計算
-            c = complex(x_min + (x_max - x_min) * x / width, y_min + (y_max - y_min) * y / height)
-            z = 0.0j
-            # マンデルブロ集合の計算
-            for i in range(iterations):
-                z = z * z + c
-                if (z.real * z.real + z.imag * z.imag) >= threshold:
-                    break
-            # 色を決定
-            if i >= iterations - 1:
-                image.putpixel((x, y), (0, 0, 0))
-            else:
-                image.putpixel((x, y), (255, 255, 255))
+        # 画像を作成
+        image = Image.new('RGB', (width, height), (0, 0, 0))
 
-    # 画像をBASE64エンコード
-    buffered = io.BytesIO()
-    image.save(buffered, format="PNG")
-    encoded_image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        for py in range(height):
+            y = py / height * (y_max - y_min) + y_min
+            for px in range(width):
+                x = px / width * (x_max - x_min) + x_min
+                z = complex(x, y)
+                v = complex(0, 0)
+                color = 0
+                for n in range(iterations):
+                    v = v * v + z
+                    if abs(v) > threshold:
+                        color = 255 - threshold * n
+                        break
+                image.putpixel((px, py), (color, color, color))
 
-    # 画像を返す
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "image/png",
-            "Parameters": json.dumps(event.get(QUERY_STRING_PARAMETERS)),
-        },
-        "body": encoded_image_data,
-        "isBase64Encoded": True,
-    }
+        # 画像をBASE64エンコード
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")
+        encoded_image_data = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+        # 画像を返す
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "image/png",
+            },
+            "body": encoded_image_data,
+            "isBase64Encoded": True,
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": str(e),
+            }),
+        }
